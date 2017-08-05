0685371da2bcef7198877e96c3b75057b611d9d4
Zeynep Dayar <zeynep.dayar@gmail.com>
2017-08-05 09:58:20 -0400

    pretty close to final

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -12,7 +12,7 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $(document).ready(function () {
-    // When user clicks "submit" button, add a new entry to firebase
+    // When user clicks "submit" button, add a new train entry to firebase
     $("#add-train").on("click", function () {
         event.preventDefault();
 
@@ -38,7 +38,7 @@ $(document).ready(function () {
 
 
     // function to calculate when the next train is and how many minutes till next train, given the first time for
-    // said train and the frequency
+    // said train in military time (HH:mm) and its frequency in minutes
     function calculateNextTrainTime(firstTime, freq) {
         // First Time (pushed back 1 year to make sure it comes before current time)
         var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
@@ -52,39 +52,64 @@ $(document).ready(function () {
         // Minutes Until Train
         var tMinutesTillTrain = freq - tRemainder;
 
-        // Next Train
+        // Next Train time calculation
         var nextTrainTime = moment().add(tMinutesTillTrain, "minutes");
 
+        // Create an object to return to caller
         var nextTrain = {
             time:  moment(nextTrainTime).format("LT"),
             minutes: tMinutesTillTrain
-        }
+        };
 
         return nextTrain;
     }
 
-    // function to update on-screen table display
-    function updateTrainsTable(trainInfo) {
-        
-
-    }
-
-    // On changes to DB, update table on the screen
+    // On changes to DB, update table on the screen -- this will be called once on first page load
+    // and then every time a new train entry is added to the DB
     database.ref().on("child_added", function (childSnapshot) {
-
         var nextTrain = calculateNextTrainTime(childSnapshot.val().firstTime, childSnapshot.val().freq);
 
         // add new row to on-screen table
-        $("#trains-table").append("<tr><td>" + childSnapshot.val().name + "</td>" +
+        $("#trains-table").append("<tr><td><b>" + childSnapshot.val().name + "</b></td>" +
             "<td>" + childSnapshot.val().destination + "</td>" +
             "<td>" + childSnapshot.val().freq + "</td>" +
-            "<td>" + nextTrain.time + "</td>" +
-            "<td>" + nextTrain.minutes + "</td></tr>");
+            "<td id=next-time-"+ childSnapshot.val().name.replace(/ /g,"-") +">" + nextTrain.time + "</td>" +
+            "<td id=next-mins-"+ childSnapshot.val().name.replace(/ /g,"-") +">" + nextTrain.minutes + "</td></tr>");
 
-        // Handle the errors
+        // update current time display in heading
+        $("#current-time").html("(" + moment().format('LT') + ")");
+
     }, function (errorObject) {
+        // Handle the errors
         console.log("Errors handled: " + errorObject.code);
     });
 
 
+    // ------------ BONUS ----------------//
+
+    // Update the trains table's last 2 dynamic columns once a minute to give accurate time for the next train
+    setInterval(updateTrainsTable, 60000);
+
+    // function to update all the next train time AND minutes to next train column values on-screen
+    function updateTrainsTable() {
+        var nextTrain;
+        var name;
+        var trains;
+
+        database.ref().once("value", function(data) {
+            trains = data.val();
+            for (var trainObj in trains) {
+                if (trains.hasOwnProperty(trainObj)) {
+                    nextTrain = calculateNextTrainTime(trains[trainObj].firstTime, trains[trainObj].freq);
+                    name = trains[trainObj].name.replace(/ /g,"-");
+
+                    $("#next-time-" + name).html(nextTrain.time);
+                    $("#next-mins-" + name).html(nextTrain.minutes);
+                }
+            }
+
+            // update current time display in heading
+            $("#current-time").html("(" + moment().format('LT') + ")");
+        });
+    }
 });
